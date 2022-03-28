209af713debd7a29f3ddff8a46b829d6de351e9c
Roman Ukhobotov <ukhobotov.rv@phystech.edu>
2022-03-28 13:44:11 +0300

    Attempts to scale the maxwell pdf

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-double count_distrib(double * hists, unsigned num_hists, double const * values, unsigned num_values, double dev_from) {
-    auto min = numeric_limits<double>::max();
-    auto max = numeric_limits<double>::min();
+float count_distrib(float * hists, unsigned num_hists, float const * values, unsigned num_values, float dev_from) {
+    auto min = numeric_limits<float>::max();
+    auto max = numeric_limits<float>::min();
     for (int j = 0; j < num_values; j++) {
         if (values[j] < min) {
             min = values[j];
@@ -16,22 +16,22 @@ double count_distrib(double * hists, unsigned num_hists, double const * values, 
             max = values[j];
         }
     }
-    auto dv = (max - min) / (double) (num_hists - 1);
+    auto dv = (max - min) / (float) (num_hists - 1);
     for (int j = 0; j < num_values; j++) {
         hists[(int) round((values[j] - min) / dv)] += 1;
     }
     return (dev_from - min) / dv;
 }
 
-double pdf(double * hists, unsigned num_hists, double const * values, unsigned num_values, double dev_from) {
+float pdf(float * hists, unsigned num_hists, float const * values, unsigned num_values, float dev_from) {
     auto value_idx = count_distrib(hists, num_hists, values, num_values, dev_from);
     for (unsigned j = 0; j < num_hists; j++) {
-        hists[j] /= (double) num_values;
+        hists[j] /= (float) num_values;
     }
     return value_idx;
 }
 
-void print_int_distrib(double const * hists, unsigned num_hists) {
+void print_int_distrib(float const * hists, unsigned num_hists) {
     for (int i = 0; i < num_hists; i++) {
         for (int j = 0; j < (int) hists[i]; j++) {
             cout << "|";
@@ -40,16 +40,16 @@ void print_int_distrib(double const * hists, unsigned num_hists) {
     }
 }
 
-double dispersion(double const * values, unsigned num_values, double dev_from) {
-    double sum = 0;
+float dispersion(float const * values, unsigned num_values, float dev_from) {
+    float sum = 0;
     for (unsigned i = 0; i < num_values; i++) {
         auto delta = values[i] - dev_from;
         sum = fma(delta, delta, sum);
     }
-    return sum / (double) num_values;
+    return sum / (float) num_values;
 }
 
-unsigned read_data(double * values) {
+unsigned read_data(float * values) {
     ifstream file("../raw.csv");
     int num_values = 0;
     if (file.is_open()) {
@@ -61,54 +61,57 @@ unsigned read_data(double * values) {
     return num_values;
 }
 
-double maxwell_pdf(double * hists, unsigned num_hists, double T) {
-    auto dev_from_idx = 0.5 * (double) (num_hists - 1);
+float maxwell_pdf(float * hists, unsigned num_hists, float scale, float T) {
+    auto max_idx = (float) (num_hists - 1);
+    auto max_dev_idx = max_idx / 2;
+    auto dv = scale / max_idx;
     for (int i = 0; i < num_hists; i++) {
-        auto v = dev_from_idx - (double) i;
-        hists[i] = exp(-v * v / T) / sqrt(T * numbers::pi);
+        auto v = ((float) (2 * i) - max_idx) * dv;
+        hists[i] = (exp(-(v * v) / T) / sqrt(T * (float) numbers::pi)) * dv;
     }
-    return dev_from_idx;
+    return max_idx / 2;
 }
 
-double mean_direct(double const * psi, double const * pdf, unsigned size) {
-    double sum = 0.;
+float mean_direct(float const * psi, float const * pdf, unsigned size) {
+    float sum = 0;
     for (auto i = 0; i < size; i++) {
         sum += pdf[i] * psi[i];
     }
     return sum;
 }
 
-double mean_fma(double const * psi, double const * pdf, unsigned size) {
-    double sum = 0.;
+float mean_fma(float const * psi, float const * pdf, unsigned size) {
+    float sum = 0;
     for (auto i = 0; i < size; i++) {
         sum = fma(pdf[i], psi[i], sum);
     }
     return sum;
 }
 
-double mean_recurse(double const * psi, double const * pdf, unsigned s) {
+float mean_recurse(float const * psi, float const * pdf, unsigned s) {
     if (s == 1) return psi[0] * pdf[0];
     auto h = s / 2;
     return mean_recurse(psi, pdf, h) + mean_recurse(&psi[h], &pdf[h], s - h);
 }
 
-double mean_kahan(double const * psi, double const * pdf, unsigned s) {
-    auto sum = 0.;
-    auto err = 0.;
+float mean_kahan(float const * psi, float const * pdf, unsigned s) {
+    float sum = 0;
+    float err = 0;
     for (auto i = 0; i < s; i++) {
         auto x = fma(pdf[i], psi[i], -err);
         auto sum_new = sum + x;
-        err = sum_new - sum - x;
+        err = (sum_new - sum) - x;
         sum = sum_new;
     }
     return sum;
 }
 
-void test_mean(double const * pdf, unsigned size, double v0, double T) {
+void test_mean(float const * pdf, unsigned size, float v0, float T) {
     cout << "mean " << v0 << " from 0 to " << size - 1 << endl;
-    auto psi = new double[size];
+    auto psi = new float[size];
     for (int i = 0; i < size; i++) {
-        psi[i] = abs(v0 - (double) i);
+        psi[i] = 1;
+//        psi[i] = abs(v0 - (float) i);
     }
     cout << "T: " << T << endl;
     cout << "expected mean: " << sqrt(T / numbers::pi) << endl;
@@ -119,15 +122,15 @@ void test_mean(double const * pdf, unsigned size, double v0, double T) {
     delete[] psi;
 }
 
-double test_data(unsigned num_hists) {
+float test_data(unsigned num_hists) {
     cout << " --- testing raw.csv --- " << endl;
 
-    double values[273];
+    float values[273];
     auto num_values = read_data(values);
-    double expected = 500;
+    float expected = 500;
     auto T = 2 * dispersion(values, num_values, expected);
 
-    auto hists = new double[num_hists] {0};
+    auto hists = new float[num_hists] {0};
     auto expected_idx = pdf(hists, num_hists, values, num_values, expected);
     test_mean(hists, num_hists, expected_idx, T);
     delete[] hists;
@@ -136,18 +139,18 @@ double test_data(unsigned num_hists) {
     return T;
 }
 
-void test_maxwell(unsigned num_hists, double T) {
+void test_maxwell(unsigned num_hists, float T) {
     cout << " --- testing maxwell distribution --- " << endl;
 
-    auto hists = new double[num_hists];
-    auto value_idx = maxwell_pdf(hists, num_hists, T);
+    auto hists = new float[num_hists];
+    auto value_idx = maxwell_pdf(hists, num_hists, 100, T);
     test_mean(hists, num_hists, value_idx, T);
 
     cout << endl;
 }
 
 int main() {
-    auto s = 100;
-    auto T = test_data(s);
-    test_maxwell(s, T);
+    auto num_hists = 20;
+    auto T = test_data(num_hists);
+    test_maxwell(num_hists, T);
 }
