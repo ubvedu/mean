b4df4a8ac443326ad7c0e40128bdb9897b1bc0d7
Roman Ukhobotov <ukhobotov.rv@phystech.edu>
2022-03-28 08:48:40 +0300

    Replaced float values with double

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-void opd(float * hists, int num_hists, float const * values, int num_values) {
+void opd(double * hists, int num_hists, double const * values, int num_values) {
     for (int j = 0; j < num_hists; j++) {
         hists[j] = 0;
     }
-    float min = numeric_limits<float>::max();
-    float max = numeric_limits<float>::min();
+    double min = numeric_limits<double>::max();
+    double max = numeric_limits<double>::min();
     for (int i = 0; i < num_values; i++) {
         if (values[i] < min) {
             min = values[i];
@@ -19,20 +19,20 @@ void opd(float * hists, int num_hists, float const * values, int num_values) {
             max = values[i];
         }
     }
-    auto dv = (max - min) / (float) (num_hists - 1);
+    auto dv = (max - min) / (double) (num_hists - 1);
     for (int i = 0; i < num_values; i++) {
         hists[(int) roundevenf((values[i] - min) / dv)] += 1;
     }
 }
 
-void pdf(float * hists, int num_hists, float const * values, int num_values) {
+void pdf(double * hists, int num_hists, double const * values, int num_values) {
     opd(hists, num_hists, values, num_values);
     for (int j = 0; j < num_hists; j++) {
-        hists[j] /= (float) num_values;
+        hists[j] /= (double) num_values;
     }
 }
 
-void print_opd(float const * hists, int num_hists) {
+void print_opd(double const * hists, int num_hists) {
     for (int i = 0; i < num_hists; i++) {
         for (int j = 0; j < (int) hists[i]; j++) {
             cout << "|";
@@ -41,18 +41,18 @@ void print_opd(float const * hists, int num_hists) {
     }
 }
 
-float std_dev(float const * values, int num_values, float mean) {
-    float std_dev = 0;
+double std_dev(double const * values, int num_values, double mean) {
+    double std_dev = 0;
     for (int i = 0; i < num_values; i++) {
         auto delta = values[i] - mean;
         std_dev = fma(delta, delta, std_dev);
     }
-    return sqrtf(std_dev / (float) num_values);
+    return sqrt(std_dev / (double) num_values);
 }
 
-float read_pdf(float * hists, int num_hists) {
+double read_pdf(double * hists, int num_hists) {
     ifstream file("../data.csv");
-    auto values = new float[300];
+    auto values = new double[300];
     int num_values = 0;
     if (file.is_open()) {
         while (file >> values[num_values]) {
@@ -65,54 +65,64 @@ float read_pdf(float * hists, int num_hists) {
     return 2 * sigma * sigma;
 }
 
-float maxwell_pdf(float * hists, int num_hists, float T) {
+double maxwell_pdf(double * hists, int num_hists, double T) {
     for (int i = 0; i < num_hists; i++) {
-        auto v = 0.5f + (float) i - 0.5f * (float) num_hists;
-        hists[i] = exp(-v * v / T) / sqrt((float) (T * numbers::pi));
+        auto v = (double) i - 0.5 * (double) (num_hists - 1);
+        hists[i] = exp(-v * v / T) / sqrt(T * numbers::pi);
     }
     return T;
 }
 
-float bias(float v, float v0) { return abs(v - v0); }
-
-float mean_direct(float const * psi, float const * pdf, unsigned size) {
-    float sum = 0.f;
+double mean_direct(double const * psi, double const * pdf, unsigned size) {
+    double sum = 0.;
     for (auto i = 0; i < size; i++) {
         sum += pdf[i] * psi[i];
     }
     return sum;
 }
 
-float mean_fma(float const * psi, float const * pdf, unsigned size) {
-    float sum = 0.f;
+double mean_fma(double const * psi, double const * pdf, unsigned size) {
+    double sum = 0.;
     for (auto i = 0; i < size; i++) {
         sum = fma(pdf[i], psi[i], sum);
     }
     return sum;
 }
 
-float mean_recursive(float const * psi, float const * pdf, unsigned size) {
-    float sum = 0.f;
-    if (size == 1) {
-        return psi[0] * pdf[0];
+double mean_rec(double const * psi, double const * pdf, unsigned s) {
+    if (s == 1) return psi[0] * pdf[0];
+    auto h = s / 2;
+    return mean_rec(psi, pdf, h) + mean_rec(&psi[h], &pdf[h], s - h);
+}
+
+double mean_kahan(double const * psi, double const * pdf, unsigned s) {
+    auto sum = 0.;
+    auto err = 0.;
+    for (auto i = 0; i < s; i++) {
+        auto x = fma(pdf[i], psi[i], -err);
+        auto sum_new = sum + x;
+        err = sum_new - sum - x;
+        sum = sum_new;
     }
-    return mean_recursive(psi, pdf, size / 2) + mean_recursive(&psi[size / 2], &pdf[size / 2], size - size / 2);
+    return sum;
 }
 
 int main() {
     auto s = 100;
-    auto distrib = new float[s];
-    auto T = maxwell_pdf(distrib, s, 2);
+    auto distrib = new double[s];
+    auto T = maxwell_pdf(distrib, s, 5);
     cout << "T: " << T << endl;
     cout << "expectation: " << sqrt(T / numbers::pi) << endl;
-    auto psi = new float[s];
+    auto psi = new double[s];
     for (int i = 0; i < s; i++) {
-        psi[i] = bias(distrib[i], 0);
+        auto v = (double) i - 0.5 * (double) (s - 1);
+        psi[i] = abs(v);
 //        psi[i] = 1;
     }
     cout << "mean direct: " << mean_direct(psi, distrib, s) << endl;
     cout << "mean by fma: " << mean_fma(psi, distrib, s) << endl;
-    cout << "mean by recursion: " << mean_recursive(psi, distrib, s) << endl;
+    cout << "mean by recursion: " << mean_rec(psi, distrib, s) << endl;
+    cout << "mean by Kahan: " << mean_kahan(psi, distrib, s) << endl;
     delete[] psi;
     delete[] distrib;
 }
